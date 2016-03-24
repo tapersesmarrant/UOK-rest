f61a0a9ce58764eebb32107cb1428195abbfe307
Nicolas Beaussart <nic.beaussart@gmail.com>
2016-03-24 08:40:26 +0100

    Edit of strUtils

--- src/main/java/fr/iutinfo/skeleton/utils/StrUtils.java
+++ src/main/java/fr/iutinfo/skeleton/utils/StrUtils.java
@@ -1,7 +1,22 @@
 package fr.iutinfo.skeleton.utils;
 
+
+import com.google.common.base.Strings;
+
 /**
  * Created by nicbe on 23/03/2016.
  */
 public class StrUtils {
+
+    public static boolean newValue( String in, String out){
+        if (Strings.nullToEmpty(in).equals(Strings.nullToEmpty(out))){
+            return false;
+        } else if (out == null){
+            return false;
+        } else if (!Strings.nullToEmpty(in).equals(Strings.nullToEmpty(out))){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
