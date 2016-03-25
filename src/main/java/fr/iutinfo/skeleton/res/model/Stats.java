0c5a3f36ed20de06be3914e0b9047b6cdb5346c9
Nicolas Beaussart <nic.beaussart@gmail.com>
2016-03-26 00:30:05 +0100

    Adding a fild to the toString

--- src/main/java/fr/iutinfo/skeleton/res/model/Stats.java
+++ src/main/java/fr/iutinfo/skeleton/res/model/Stats.java
@@ -54,7 +54,7 @@ public class Stats {
     public String toString() {
         return MoreObjects.toStringHelper(this)
                 .add("nmbUsers", nmbUsers)
+                .add("nmbEvents", nmbEvents)
                 .toString();
     }
-
 }
